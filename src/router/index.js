d183aae831cafbb873527c6728b0ef61bb37755e
luisfcv97 <luisfcv97@gmail.com>
2020-04-29 16:21:28 -0500

    first screen

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,25 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "LandsPage",
+    component: () => import("../views/loginRegister/LandsPage.vue"),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/loginRegister/Login.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/loginRegister/LandsPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    <div>
+      <img src="./img/person1.png" height="500" width="600" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandsPage"
+};
+</script>
+
+<style scoped>
+.main {
+  background-image: url("./img/LandsPage.jpg");
+  background-blend-mode: color;
+  background-size: 100% 100%;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+</style>
