08030a34ddc58e9809e7341c094a50ab949833a0
Kevin Balanta <kevin232454@hotmail.com>
2020-05-21 13:37:45 -0500

    Natural Person Views
    The filter of debts and welcome page

--- src/views/naturalPerson/InversionNP.vue
+++ src/views/naturalPerson/InversionNP.vue
@@ -3,7 +3,7 @@
     <v-container grid-list-md>
       <v-layout row wrap>
         <v-flex xs6>
-          <v-text-field label="Search" append-icon="search"></v-text-field>
+          <v-text-field v-model="label" label="Search" append-icon="search"></v-text-field>
         </v-flex>
         <v-flex xs2>
           <img src="../../assets/logo.png" />
@@ -18,14 +18,14 @@
         <v-card
           class="mx-auto mb-6 pb-2"
           max-width="344"
-          v-for="inversion in inversions"
+          v-for="inversion in filtering"
           :key="inversion.id"
         >
           <v-img src="../../assets/agriculture.jpg" height="200px"></v-img>
 
-          <v-card-title>Publicado por: {{ inversion.owner}}</v-card-title>
-          <v-card-title class="subtitle-1 font-weight-medium">Deudor: {{ inversion.owner_debt}}</v-card-title>
-          <v-card-title class="subtitle-1 font-weight-medium">Valor: {{ inversion.value}}</v-card-title>
+          <v-card-title>Publicado por: {{ inversion.userid}}</v-card-title>
+          <v-card-title class="subtitle-1 font-weight-medium">Deudor: {{ inversion.debtor}}</v-card-title>
+          <v-card-title class="subtitle-1 font-weight-medium">Valor: {{ inversion.valuedebt}}</v-card-title>
 
           <v-card-actions>
             <v-btn color="blue" text-color="white" class="white--text">Ofertar</v-btn>
@@ -41,50 +41,35 @@
 </template>
 
 <script>
+import axios from "@/plugins/axios";
 export default {
   name: "InversionNP",
   data() {
     return {
-      inversions: [
-        {
-          id: 0,
-          owner: "juan pablo",
-          owner_debt: "La 13",
-          value: 50000000
-        },
-        {
-          id: 0,
-          owner: "Luis Cruces",
-          owner_debt: "La 14",
-          value: 50000000
-        },
-        {
-          id: 0,
-          owner: "Jaiver Orozco",
-          owner_debt: "Exito S.A",
-          value: 50000000
-        },
-        {
-          id: 0,
-          owner: "juan pablo",
-          owner_debt: "La 13",
-          value: 50000000
-        },
-        {
-          id: 0,
-          owner: "Luis Cruces",
-          owner_debt: "La 14",
-          value: 50000000
-        },
-        {
-          id: 0,
-          owner: "Jaiver Orozco",
-          owner_debt: "Exito S.A",
-          value: 50000000
-        }
-      ]
+      inversions: [],
+      label:""
     };
-  }
+  },
+  methods:{
+    async init(){
+      try{
+           var response = await axios.get(
+              "/debts/debt"
+            );
+            console.log(response);
+            this.inversions = this.inversions.concat(response.data.data);
+        }catch(error){
+
+        }
+    },
+  },
+  computed: {
+    filtering() {
+      return this.inversions.filter(el => el.category.match(this.label))
+    }
+  },
+  mounted: function () { this.init() },
+  
 };
 </script>
 
