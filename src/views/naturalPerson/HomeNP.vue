b337f2aa4387bc674399f709fa08c31c69c02af8
Kevin Balanta <kevin232454@hotmail.com>
2020-05-21 08:07:47 -0500

    Auto stash before merge of "develop" and "luisVidal97/develop"

--- src/views/naturalPerson/HomeNP.vue
+++ src/views/naturalPerson/HomeNP.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>Bienvenido persona naturals</h1>
+  <v-container class="pantalla">
+    <v-card width="500" class="column1 mx-12 mt-8 px-4" color="#2F303A">
+      <v-card-title class="justify-center">
+        <h1 class="display-1 white--text font-weight-medium mt-11">Bienvenido</h1>
+      </v-card-title>
+      <v-spacer></v-spacer>
+      <v-card-subtitle class="justify-center">
+        <p
+          class="title white--text text-center"
+        >Ahora podras realizar busquedas de las diferentes carteras que estan siendo subastadas en estos momentos.</p>
+      </v-card-subtitle>
+
+      <v-card-actions class="mb-4">
+        <h1 class="display-2 white--text text-center mb-8">No esperes más, inicia Ya!</h1>
+      </v-card-actions>
+      <v-btn class="white--text" color="deep-purple accent-4 mx-10" to="/listProducts">Vamos!</v-btn>
+    </v-card>
+    <div class="column2 mt-2">
+      <img src="./img/person2.png" height="500" width="400" />
+    </div>
+  </v-container>
 </template>
 
 <script>
@@ -8,5 +28,38 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.pantalla {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-left: 40px;
+  margin-right: 40px;
+  height: 500px;
+}
+.column1 {
+  display: flex;
+  width: 45%;
+  flex-direction: column;
+  align-content: center;
+  border-radius: 60px;
+}
+.column2 {
+  display: flex;
+  width: 50%;
+  flex-direction: column;
+  margin-top: 10%;
+}
+
+.row1 {
+  display: flex;
+  width: 50%;
+  flex-direction: row;
+}
+.row2 {
+  display: flex;
+  width: 50%;
+  flex-direction: row;
+  margin-top: 30px;
+}
 </style>
---
+++ src/views/naturalPerson/InversionNP.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-container>
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-flex xs6>
+          <v-text-field label="Search" append-icon="search"></v-text-field>
+        </v-flex>
+        <v-flex xs2>
+          <img src="../../assets/logo.png" />
+        </v-flex>
+        <v-flex xs2>
+          <h1>BuyDebt</h1>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-card
+          class="mx-auto mb-6 pb-2"
+          max-width="344"
+          v-for="inversion in inversions"
+          :key="inversion.id"
+        >
+          <v-img src="../../assets/agriculture.jpg" height="200px"></v-img>
+
+          <v-card-title>Publicado por: {{ inversion.owner}}</v-card-title>
+          <v-card-title class="subtitle-1 font-weight-medium">Deudor: {{ inversion.owner_debt}}</v-card-title>
+          <v-card-title class="subtitle-1 font-weight-medium">Valor: {{ inversion.value}}</v-card-title>
+
+          <v-card-actions>
+            <v-btn color="blue" text-color="white" class="white--text">Ofertar</v-btn>
+
+            <v-btn color="dark green">Ver más</v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-layout>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "InversionNP",
+  data() {
+    return {
+      inversions: [
+        {
+          id: 0,
+          owner: "juan pablo",
+          owner_debt: "La 13",
+          value: 50000000
+        },
+        {
+          id: 0,
+          owner: "Luis Cruces",
+          owner_debt: "La 14",
+          value: 50000000
+        },
+        {
+          id: 0,
+          owner: "Jaiver Orozco",
+          owner_debt: "Exito S.A",
+          value: 50000000
+        },
+        {
+          id: 0,
+          owner: "juan pablo",
+          owner_debt: "La 13",
+          value: 50000000
+        },
+        {
+          id: 0,
+          owner: "Luis Cruces",
+          owner_debt: "La 14",
+          value: 50000000
+        },
+        {
+          id: 0,
+          owner: "Jaiver Orozco",
+          owner_debt: "Exito S.A",
+          value: 50000000
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
